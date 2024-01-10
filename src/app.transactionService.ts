53ff0f6341d3fb433ad5e2a57a7145914969860d
Dominik Duda <dominikduda191@gmail.com>
2024-01-10 14:11:44 +0100

    comments

--- src/app.transactionService.ts
+++ src/app.transactionService.ts
@@ -1,87 +1,146 @@
 const { Web3 } = require('web3');
-const TransactionData = require("./app.transactionData");
-const TransactionValue = require("./app.transactionValue");
+const TransactionData = require('./app.transactionData');
+const TransactionValue = require('./app.transactionValue');
 
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
 @Injectable()
-export class TransactionService{
-	constructor(private readonly httpService: HttpService) {}
+export class TransactionService {
+  constructor(private readonly httpService: HttpService) {}
 
-web3 = new Web3(new Web3.providers.WebsocketProvider('wss://polygon-mumbai.g.alchemy.com/v2/MA3xOeLHGBEOPX0i2kVaqnMs5phMsJvX'));
+  web3 = new Web3(
+    new Web3.providers.WebsocketProvider(
+      'wss://polygon-mumbai.g.alchemy.com/v2/MA3xOeLHGBEOPX0i2kVaqnMs5phMsJvX',
+    ),
+  ); // todo env variable
 
-getTransactionType(transfer:any, address){
-    if(transfer.from.toLowerCase() === address.toLowerCase()) return "Withdraw";
-    if(transfer.to.toLowerCase() === address.toLowerCase()) return "Deposit";
-    return "Observed address not taking part in given transaction";
-}
+  getTransactionType(transfer: any, address) {
+    if (transfer.from.toLowerCase() === address.toLowerCase())
+      return 'Withdraw';
+    if (transfer.to.toLowerCase() === address.toLowerCase()) return 'Deposit';
+    throw new InternalServerErrorException(
+      'Internal Error : web3.eth.subscribe catched event not linked to given address : ' +
+        address,
+    );
+  }
 
-async getTokenType(tokenId,contractAddress){
-  	const contract = new this.web3.eth.Contract(TransactionData.getAbi(), contractAddress);
-  	const ownerOf = await contract.methods.ownerOf(tokenId).call();
-  	if(ownerOf) return "ERC-721";
-  	const balanceOf = await contract.methods.balanceOf(tokenId).call();
-  	if(balanceOf) return "ERC-1155";
-  	return "Unknown Token Type";
-}
+  async getTokenType() {
+    const abiFunctions = TransactionData.getAbi().filter(
+      (item) => item.type.toLowerCase() === 'function'.toLowerCase(),
+    );
+    const abiEvents = TransactionData.getAbi().filter(
+      (item) => item.type.toLowerCase() === 'event'.toLowerCase(),
+    );
 
-async getFromAndTo(transaction:any){
-	const decoded = this.web3.eth.abi.decodeParameters(TransactionData.getAbi()[15].inputs, transaction.input.slice(10));
-	const transfer = {
-        from : decoded.from as string,
-		to : decoded.to as string,
+    const hasTransfer = abiFunctions.some(
+      (fn) => fn.name.toLowerCase() === 'transferFrom'.toLowerCase(),
+    ); //ERC 721 specyfic functions
+    const hasTransferEvent = abiEvents.some(
+      (ev) => ev.name.toLowerCase() === 'transfer'.toLowerCase(),
+    );
+
+    const hasTransferBatch = abiFunctions.some(
+      (fn) => fn.name.toLowerCase() === 'safeTransferFromBatch'.toLowerCase(),
+    ); //ERC 1155 specyfic functions
+    const hasTransferBatchEvent = abiEvents.some(
+      (ev) => ev.name.toLowerCase() === 'transferBatch'.toLowerCase(),
+    );
+
+    if (hasTransfer && hasTransferEvent) {
+      return 'ERC-721';
+    } else if (hasTransferBatch && hasTransferBatchEvent) {
+      return 'ERC-1155';
+    } else {
+      return 'Unknown Token Type';
+    }
+  }
+
+  async getFromAndTo(transaction: any) {
+    const decoded = this.web3.eth.abi.decodeParameters(
+      TransactionData.getAbi()[15].inputs,
+      transaction.input.slice(10),
+    );
+    const transfer = {
+      from: decoded.from as string,
+      to: decoded.to as string,
     };
-	return transfer;
-}
+    return transfer;
+  }
 
-async getTokenId(txHash){
-	const data = await this.web3.eth.getTransactionReceipt(txHash);
-	const logs = data.logs;
-	return this.web3.utils.hexToNumber(logs[0].topics[3]);
-}
+  async getTokenId(txHash) {
+    const data = await this.web3.eth.getTransactionReceipt(txHash);
+    const logs = data.logs;
+    return this.web3.utils.hexToNumber(logs[0].topics[3]);
+  }
 
-async getTimestamp(blockNumber){
-	const block = await this.web3.eth.getBlock(blockNumber);
+  async getTimestamp(blockNumber) {
+    const block = await this.web3.eth.getBlock(blockNumber);
     return block.timestamp;
-}
+  }
 
-async createTransaction(output, contractAddress:string, observedAddress:string){
-		const tx = await this.web3.eth.getTransaction(output.transactionHash);// token index to ma być to czy to ma być ten index tokena mumbai np?
-		const tokenId = await this.getTokenId(tx.hash);
-		const transfer = await this.getFromAndTo(tx);
-		const transactionType = this.getTransactionType(transfer, observedAddress);
-		const timestamp = await this.getTimestamp(tx.blockNumber);
-		const tokenType = await this.getTokenType(tokenId, contractAddress);
-		const transaction = new TransactionValue(contractAddress, tx.hash, transfer.from, transfer.to, transactionType, timestamp, tokenId, tokenType);
-		const transactionString = transaction.toString();
-		this.httpService
-      	.post('https://webhook.site/d6c7a688-f861-423e-8cfd-1e184ad631c0', transactionString)
-      	.subscribe({
+  async createTransaction(output, observedAddress: string) {
+    const tx = await this.web3.eth.getTransaction(output.transactionHash); // token index to ma być to czy to ma być ten index tokena mumbai np?
+    const logs = tx.logs;
+    console.log(
+      'Tx : ' +
+        JSON.stringify(tx, (_, v) =>
+          typeof v === 'bigint' ? v.toString() : v,
+        ),
+    );
+    console.log('Logs : ' + logs);
+    const tokenId = await this.getTokenId(tx.hash);
+    const transfer = await this.getFromAndTo(tx);
+    const transactionType = this.getTransactionType(transfer, observedAddress); //todo enum
+    const timestamp = await this.getTimestamp(tx.blockNumber);
+    const tokenType = await this.getTokenType(); //todo enum
+    console.log('Transaction Type : ' + tokenType);
+    const transaction = new TransactionValue(
+      tx.hash,
+      transfer.from,
+      transfer.to,
+      transactionType,
+      timestamp,
+      tokenId,
+      tokenType,
+    );
+    const transactionString = transaction.toString();
+    this.httpService
+      .post(
+        'https://webhook.site/d6c7a688-f861-423e-8cfd-1e184ad631c0',
+        transactionString,
+      )
+      .subscribe({
         complete: () => {
           console.log('completed');
         },
         error: (err) => {
-          console.log("Error occured while sending data to webhook : " + err)
+          console.log('Error occured while sending data to webhook : ' + err);
         },
-      	});
-		return transaction;
-}
+      });
+    return transaction;
+  }
 
-
-async listenTransactions(observedWallet:string){
-console.log('Listening to trasnactions on contractAddress : ' + TransactionData.getContractAddress());
-this.web3.eth.subscribe('logs', {address:TransactionData.getContractAddress()}, function (err, res) {
-  console.log(res);
-  console.log(err);
-}).then((data) => data.on('data', async (output) => {
-	try {
-		const result = this.createTransaction(output, TransactionData.getContractAddress(), observedWallet);
-		return result;
-      		}
-			catch (err) {
-      console.error(`Error in event callback: ${err}`);
-  	}
-}));
-}
+  async listenTransactions(observedWallet: string) {
+    console.log('Listening on transaction on address : ' + observedWallet);
+    this.web3.eth
+      .subscribe(
+        'logs',
+        { address: TransactionData.getContractAddress() },
+        function (err, res) {
+          console.log(res);
+          console.log(err);
+        },
+      )
+      .then((data) =>
+        data.on('data', async (output) => {
+          try {
+            const result = this.createTransaction(output, observedWallet);
+            return result;
+          } catch (err) {
+            console.error(`Error in event callback: ${err}`);
+          }
+        }),
+      );
+  }
 }
--- src/app.transactionValue.ts
+++ src/app.transactionValue.ts
@@ -1,38 +1,58 @@
-export class TransactionValue{
+export class TransactionValue {
+  transactionHash: string;
+  from: string;
+  to: string;
+  transactionType: string;
+  timestamp: string;
+  tokenIndex: string;
+  tokenType: string;
 
-    observedContractAddress:string;
-    transactionHash:string;
-    from:string;
-    to:string;
-    transactionType:string;
-    timestamp:string;
-    tokenIndex:string;
-    tokenType:string;
+  constructor(
+    transactionHash,
+    from,
+    to,
+    transactionType,
+    timestamp,
+    tokenIndex,
+    tokenType,
+  ) {
+    this.transactionHash = transactionHash;
+    this.from = from;
+    this.to = to;
+    this.transactionType = transactionType;
+    this.timestamp = timestamp;
+    this.tokenIndex = tokenIndex;
+    this.tokenType = tokenType;
+  }
 
-    constructor(observedContractAddress, transactionHash, from, to, transactionType, timestamp, tokenIndex, tokenType){
-        this.observedContractAddress = observedContractAddress;
-        this.transactionHash = transactionHash;
-        this.from = from;
-        this.to = to;
-        this.transactionType = transactionType;
-        this.timestamp = timestamp;
-        this.tokenIndex = tokenIndex;
-        this.tokenType = tokenType
-    }
-
-    toString() :string{ // todo dopytać jak się powinno concatenować stringi w js
-        const response = "Transaction : {\n  " + 
-        "observedContractAddress : " + this.observedContractAddress + "\n  " + 
-        "transactionHash : " + this.transactionHash + "\n  " +
-        "from : " + this.from + "\n  " +
-        "to : " + this.to + "\n  " +
-        "transactionType : " + this.transactionType + "\n  " + 
-        "timestamp : " + this.timestamp + "\n  " + // todo dopytać jaki ma być ten timestamp(czy to ma być data czy long)
-        "tokenIndex : " + this.tokenIndex + "\n  " + 
-        "tokenType : " + this.tokenType + "\n}";
-        console.log(response);
-        return response;
-    }
+  toString(): string {
+    // todo dopytać jak się powinno concatenować stringi w js
+    const response =
+      'Transaction : {\n  ' +
+      'transactionHash : ' +
+      this.transactionHash +
+      '\n  ' +
+      'from : ' +
+      this.from +
+      '\n  ' +
+      'to : ' +
+      this.to +
+      '\n  ' +
+      'transactionType : ' +
+      this.transactionType +
+      '\n  ' +
+      'timestamp : ' +
+      this.timestamp +
+      '\n  ' + // todo dopytać jaki ma być ten timestamp(czy to ma być data czy long)
+      'tokenIndex : ' +
+      this.tokenIndex +
+      '\n  ' +
+      'tokenType : ' +
+      this.tokenType +
+      '\n}';
+    console.log(response);
+    return response;
+  }
 }
 
 module.exports = TransactionValue;
